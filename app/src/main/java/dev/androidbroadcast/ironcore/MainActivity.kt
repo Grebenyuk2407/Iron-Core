977d1fc41142bb792ac37f3f9a517a25d4df7487
GAV <antongrebenyuk1231@gmail.com>
2024-09-23 18:24:31 +0300

    create bottom naviation menu

--- app/src/main/java/dev/androidbroadcast/ironcore/MainActivity.kt
+++ app/src/main/java/dev/androidbroadcast/ironcore/MainActivity.kt
@@ -1,11 +1,14 @@
 package dev.androidbroadcast.ironcore
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +20,17 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Если тебе нужно добавить нижнее меню или тулбар, здесь ты можешь связать его с навигацией:
-        // Например, если есть BottomNavigationView
-        // val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
-        // NavigationUI.setupWithNavController(bottomNav, navController)
+        // Привязка BottomNavigationView к NavController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        NavigationUI.setupWithNavController(bottomNav, navController)
+
+        // Если нужно, обработай навигацию по фрагментам и настроить возвращение в предыдущие экраны.
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.fragmentLogin || destination.id == R.id.fragmentRegistration) {
+                bottomNav.visibility = View.GONE // Скрываем нижнее меню на экранах входа и регистрации
+            } else {
+                bottomNav.visibility = View.VISIBLE // Показываем меню на других экранах
+            }
+        }
     }
 }
